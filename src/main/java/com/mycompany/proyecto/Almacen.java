b6523e7b6fa9086464e13d2425d42e6c83ce16bb
genecc15 <36336252+genecc15@users.noreply.github.com>
2021-05-06 00:05:31 -0600

    Almacenamiento Finalizado

--- src/main/java/com/mycompany/proyecto/Almacen.java
+++ src/main/java/com/mycompany/proyecto/Almacen.java
@@ -21,15 +21,15 @@ public class Almacen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Map<String, String>  Etiquetas = new HashMap<String, String>();
-        Map<String, Integer> Ocurrencias = new HashMap<String, Integer>();
+        Map<String, Map<String, Integer>>  Etiquetas = new HashMap<String, Map<String, Integer>>();
+//        Map<String, Integer> Ocurrencias = new HashMap<String, Integer>();
         List<String> ejemploLista = new ArrayList();
-        List<String> ejemploLista2 = new ArrayList();
+//        List<String> ejemploLista2 = new ArrayList();
         String[] Frases;
         String[] Palabras;
         String Probando = "Hola como estas|Español";
-        String Prueba = "Bye my friends estas|Ingles";
-        String Prueba2 = "Bye my friends estas|Ingles";
+        String Prueba = "Bye my tio|Ingles";
+        String Prueba2 = "Bye my tio|Ingles";
         ejemploLista.add(Prueba);
         ejemploLista.add(Prueba2);
         ejemploLista.add(Probando);
@@ -38,23 +38,33 @@ public class Almacen {
             Frases = str.split("\\|");
             String Tag = Frases[1];
             String Frase = Frases[0];
-            ejemploLista2.add(Frase);
-            Etiquetas.put(Tag, Frase);
-        }
-        for ( String Palabra : ejemploLista2 )
-            {
-                Palabras = Palabra.split(" ");
-                System.out.println(Palabras.toString());
+            
+            if(Etiquetas.containsKey(Tag)){
+                
+                Palabras = Frase.split(" ");
                 for (int i = 0; i < Palabras.length; i++) {
-                    Integer oldCount = Ocurrencias.get(Palabras[i]);
+                    Integer oldCount = Etiquetas.get(Tag).get(Palabras[i]);
                     if(oldCount == null)
                     {
                         oldCount = 0;
                     }
-                    Ocurrencias.put(Palabras[i], oldCount + 1);
+                    Etiquetas.get(Tag).put(Palabras[i], oldCount +1);
                 }
             }
-                System.out.println(Ocurrencias);
+            else{
+                Etiquetas.put(Tag, new HashMap<String, Integer>());
+                Palabras = Frase.split(" ");
+                for (int i = 0; i < Palabras.length; i++) {
+                    Integer oldCount = Etiquetas.get(Tag).get(Palabras[i]);
+                    if(oldCount == null)
+                    {
+                        oldCount = 0;
+                    }
+                    Etiquetas.get(Tag).put(Palabras[i], oldCount +1);
+                }
+            }
+        }
+                System.out.println(Etiquetas);
     }
     
 }
