de829f313300ecf19392d5bff3dd4ba8ac7da307
genecc15 <36336252+genecc15@users.noreply.github.com>
2021-05-06 00:42:29 -0600

    Almacenado con Lectura de Archivo

--- src/main/java/com/mycompany/proyecto/Almacen.java
+++ src/main/java/com/mycompany/proyecto/Almacen.java
@@ -5,42 +5,50 @@
  */
 package com.mycompany.proyecto;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author genec
  */
 public class Almacen {
-
+    static BufferedReader lector;
+        static String linea;
+        public static ArrayList<String> lista = new ArrayList();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Conteo();
+    }
+    public static void Conteo(){
+    leer("C:\\Users\\genec\\OneDrive\\Escritorio\\test.csv");
         Map<String, Map<String, Integer>>  Etiquetas = new HashMap<String, Map<String, Integer>>();
-//        Map<String, Integer> Ocurrencias = new HashMap<String, Integer>();
-        List<String> ejemploLista = new ArrayList();
-//        List<String> ejemploLista2 = new ArrayList();
         String[] Frases;
         String[] Palabras;
-        String Probando = "Hola como estas|Español";
-        String Prueba = "Bye my tio|Ingles";
-        String Prueba2 = "Bye my tio|Ingles";
-        ejemploLista.add(Prueba);
-        ejemploLista.add(Prueba2);
-        ejemploLista.add(Probando);
-        for(String str : ejemploLista)
+        for(String str : lista)
         {
             Frases = str.split("\\|");
             String Tag = Frases[1];
             String Frase = Frases[0];
             
             if(Etiquetas.containsKey(Tag)){
-                
+                Frase = Frase.replace('.', ' ');
+                Frase = Frase.replace('?', ' ');
+                Frase = Frase.replace('¿', ' ');
+                Frase = Frase.replace('!', ' ');
+                Frase = Frase.replace('¡', ' ');
+                Frase = Frase.replace('(', ' ');
+                Frase = Frase.replace(')', ' ');
+                Frase = Frase.replace(',', ' ');
+                Frase = Frase.replace('"', ' ');
                 Palabras = Frase.split(" ");
                 for (int i = 0; i < Palabras.length; i++) {
                     Integer oldCount = Etiquetas.get(Tag).get(Palabras[i]);
@@ -53,6 +61,15 @@ public class Almacen {
             }
             else{
                 Etiquetas.put(Tag, new HashMap<String, Integer>());
+                Frase = Frase.replace('.', ' ');
+                Frase = Frase.replace('?', ' ');
+                Frase = Frase.replace('¿', ' ');
+                Frase = Frase.replace('!', ' ');
+                Frase = Frase.replace('¡', ' ');
+                Frase = Frase.replace('(', ' ');
+                Frase = Frase.replace(')', ' ');
+                Frase = Frase.replace(',', ' ');
+                Frase = Frase.replace('"', ' ');
                 Palabras = Frase.split(" ");
                 for (int i = 0; i < Palabras.length; i++) {
                     Integer oldCount = Etiquetas.get(Tag).get(Palabras[i]);
@@ -65,6 +82,17 @@ public class Almacen {
             }
         }
                 System.out.println(Etiquetas);
+    }
+    public static void leer(String nombreArchivo){
+        try{
+            lector = new BufferedReader(new FileReader(nombreArchivo));
+            while((linea=lector.readLine()) != null){
+                lista.add(linea);
+            }
+            lector.close();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
     }
     
 }
